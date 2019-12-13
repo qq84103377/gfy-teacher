195cfe362ae8d25bcf7f8fa3eb407d27d66028d1
fengqiao <1147655443@qq.com>
2019-12-13 15:15:32 +0800

    优化

--- src/views/mine/shop/goodsDetail.vue
+++ src/views/mine/shop/goodsDetail.vue
@@ -58,6 +58,7 @@
 <script>
   import {getGoodsList, addMallCollectInfo, cancelMallCollectInfo, addExchangeApplyGoodsInfo,getUserCounterList} from "@/api/mine";
   import {ImagePreview} from 'vant';
+  import eventBus from "@/utils/eventBus";
 
   export default {
     name: "goodsDetail",
@@ -166,6 +167,7 @@
           // console.log('cancelMallCollectInfo', res)
           if (res.flag) {
             this.isCollect = false;
+            eventBus.$emit("collectListRefresh", true);
             this.$toast.success('取消收藏成功！')
           }
         })
