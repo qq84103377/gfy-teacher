abbe17fd35cd8188c1b5080dee46eeea83b43299
fengqiao <1147655443@qq.com>
2020-01-22 10:15:35 +0800

    更新积分和兑换规则内容样式

--- src/views/mine/shop/convertRule.vue
+++ src/views/mine/shop/convertRule.vue
@@ -5,21 +5,21 @@
     </van-nav-bar>
     <div class="convertRule-body">
       <div class="item">
-        <h3>规则一</h3>
+        <h3>规则一.</h3>
         <p>
-          1.除假期外，当月兑换的奖品通过审核，会在下个月由工作人员联系，进行兑换
+          除假期外，当月兑换的奖品通过审核，会在下个月由该教研员直接联系，进行兑换。
         </p>
       </div>
       <div class="item">
-        <h3>规则二</h3>
+        <h3>规则二.</h3>
         <p>
-          2.奖品质量问题，请收到奖品时及时检查，如有问题，请直接联系工作人员进行更换
+          奖品质量问题，请收到奖品时及时检查，如有问题，请直接联系该校的教研员进行更换。
         </p>
       </div>
       <div class="item">
-        <h3>规则三</h3>
+        <h3>规则三.</h3>
         <p>
-          3.如果在兑换奖品过程中有任何问题，请联系工作人员。
+          如果在兑换奖品过程中有任何问题，请联系该校的教研员。
         </p>
       </div>
 
@@ -47,22 +47,25 @@
     }
 
     &-body {
-      padding: 15px;
+      padding: 20px;
 
       .item {
-        margin-bottom: 20px;
+        margin-bottom: 29px;
 
         h3 {
-          font-size: 16px;
-          color: #000000;
-          font-weight: 600;
-          padding: 10px 0;
+          font-size: 15px;
+          font-weight: 500;
+          border-left: 3px solid #39F0DD;
+          padding-left: 7px;
+          color: #000;
+          margin-bottom: 5px;
         }
 
         p {
           font-size: 14px;
           color: #333333;
           line-height: 22px;
+          padding-left: 7px;
         }
       }
 
