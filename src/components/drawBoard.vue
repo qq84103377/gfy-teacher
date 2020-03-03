7bfa65d9d652a11154beabb7e27df6a964783ef7
yk_fung <341789336@qq.com>
2020-03-03 17:49:49 +0800

    fixed 涂鸦图片上传压缩/发任务

--- src/components/drawBoard.vue
+++ src/components/drawBoard.vue
@@ -193,9 +193,9 @@ export default {
       let b = this.$refs['canvas'].height
       this.offCtx.clearRect(0, 0, this.$refs['canvas'].width, this.$refs['canvas'].height); // 先清除画布
       this.changeImg = new Image();
-      this.changeImg.setAttribute("crossOrigin", 'anonymous');
-      this.changeImg.src = changeValue + '&' + Math.random();
-      // this.changeImg.src = changeValue;
+      // this.changeImg.setAttribute("crossOrigin", 'anonymous');
+      // this.changeImg.src = changeValue + '&' + Math.random();
+      this.changeImg.src = changeValue;
       this.changeImg.onload = () => {
         console.log(changeValue);
         this.offCtx.drawImage(this.changeImg, 0, 0, this.$refs['canvas'].width, this.$refs['canvas'].height);
